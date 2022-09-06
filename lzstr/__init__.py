ec3a6c5aa5064976409be20a71c5a82b6d5bafef
Johannes Bauer <joe@johannes-bauer.com>
2022-09-06 21:10:00 +0200

    Release v0.0.2
    Releasing this version as v0.0.2, as it's now at least usable for my
    purposes.

--- lzstr/__init__.py
+++ lzstr/__init__.py
@@ -23,4 +23,4 @@ from .BitString import BitString
 from .LZString import LZStringDecompressor, LZStringCompressor
 from .Exceptions import LZStringDecompressionException
 
-VERSION = "0.0.2-rc0"
+VERSION = "0.0.2"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 setuptools.setup(
 	name = "lzstr",
 	packages = setuptools.find_packages(),
-	version = "0.0.1",
+	version = "0.0.2",
 	license = "gpl-3.0",
 	description = "Native Python implementation of LZString string compression",
 	long_description = long_description,
@@ -14,7 +14,7 @@ setuptools.setup(
 	author = "Johannes Bauer",
 	author_email = "joe@johannes-bauer.com",
 	url = "https://github.com/johndoe31415/pylzstring",
-	download_url = "https://github.com/johndoe31415/pylzstring/archive/v0.0.1.tar.gz",
+	download_url = "https://github.com/johndoe31415/pylzstring/archive/v0.0.2.tar.gz",
 	keywords = [ "python", "lzstring" ],
 	classifiers = [
 		"Development Status :: 3 - Alpha",
